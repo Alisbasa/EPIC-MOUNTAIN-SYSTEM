ece1448baf8d5fd5e0efc5397377e471f0898449
erickrody02 <67437140+erickrody02@users.noreply.github.com>
2020-07-08 11:53:16 -0600

    Cambio en clase RellenarG

--- JavaApplication19/src/javaapplication19/RellenarG.java
+++ JavaApplication19/src/javaapplication19/RellenarG.java
@@ -24,131 +24,255 @@ import javaapplication19.Libros;
  * @author Alex
  */
 public class RellenarG {
-    JPanel rellenarVentas =new JPanel();
-    JPanel rellenarInversion =new JPanel();
-    JPanel rellenarImpuesto =new JPanel();
-    JPanel rellenarCredito =new JPanel();
-    JLabel iconoVentas = new JLabel();
-    JLabel iconoCredito = new JLabel();
-    JLabel iconoInversion = new JLabel();
+    
+    JPanel rellenarDesarrollo =new JPanel();
+    JPanel rellenarDeudasPagar =new JPanel();
+    JPanel rellenarImpuestoG =new JPanel();
+    JPanel rellenarMantenimiento =new JPanel();
+    JPanel rellenarSuministros =new JPanel();
+    JPanel rellenarPubliidad =new JPanel();
+    JPanel rellenarTransporte =new JPanel();
+    JPanel rellenarHonorarios =new JPanel();
+    JPanel rellenarReducInv =new JPanel();
+    
+    JLabel iconoDesarrollo = new JLabel();
+    JLabel iconoDeudasPagar = new JLabel();
     JLabel iconoImpuesto = new JLabel();
-    JLabel iconoOkV = new JLabel();
-    JLabel iconoOkC = new JLabel();
-    JLabel iconoOkI = new JLabel();
-    JLabel iconoOkImp = new JLabel();
-    String monto2;
-    JTextField monto;
-    JComboBox inventario;
-    JComboBox plataformacb;
-    JTextField inversor;
-    JTextField montoC;
-    JTextField montoI;
-    JTextField inversorI;
+    JLabel iconoMantenimiento = new JLabel();
+    JLabel iconoSuministros = new JLabel();
+    JLabel iconoPublicidad = new JLabel();
+    JLabel iconoTransporte = new JLabel();
+    JLabel iconoHonorarios = new JLabel();
+    JLabel iconoReducInv = new JLabel();
+    
+    JComboBox desarrollo;
+    JTextField deudas;
     JTextField impuesto;
-    JTextField montoImp;
+    JTextField mantenimiento;
+    JTextField suministros;
+    JTextField publicidad;
+    JTextField transporte;
+    JTextField honorarios;
+    JComboBox reducInv;
+    
+    JTextField montoDes;
+    JTextField montoDeuda;
+    JTextField montoImpuesto;
+    JTextField montoMant;
+    JTextField montoSum;
+    JTextField montoPublicidad;
+    JTextField montoTrans;
+    JTextField montoHonor;
+    JTextField montoReduc;
+    
+    JLabel iconoOkDesarrollo = new JLabel();
+    JLabel iconoOkDeudas = new JLabel();
+    JLabel iconoOkImpuesto = new JLabel();
+    JLabel iconoOkMantenimiento = new JLabel();
+    JLabel iconoOkSuministros = new JLabel();
+    JLabel iconoOkPublicidad = new JLabel();
+    JLabel iconoOkTransporte = new JLabel();
+    JLabel iconoOkHonorarios = new JLabel();
+    JLabel iconoOkReducInv = new JLabel();
+     
     
     
     
-    private Ventas venta;
     
-    
-    
-    
-     public  JPanel rellenarVentas(){
-        rellenarVentas.removeAll();
+     public  JPanel rellenarDesarrollo(){
+        rellenarDesarrollo.removeAll();
         
-        rellenarVentas.setBackground(Color.white);
-        Iconos.scaleImage("Ventas", iconoVentas, 40);
-        rellenarVentas.add(iconoVentas);
-        String[] listaInventario={"inventario","Bici chingona","Bici Chingona 2","Bici Chingona 3"};
-        String[] listaPlataformas={"ML","fACEBOOK","Amazon","Shopiffy"};
+        rellenarDesarrollo.setBackground(Color.white);
+        Iconos.scaleImage("Ventas", iconoDesarrollo, 40);
+        rellenarDesarrollo.add(iconoDesarrollo);
+        String[] listaDesarrollo={"Mobiliario, Equisde"};
        
-        inventario = new JComboBox(listaInventario);
+        desarrollo = new JComboBox(listaDesarrollo);
        
-        plataformacb = new JComboBox(listaPlataformas);
-        
-        monto = new JTextField("$");
+        montoDes = new JTextField("$");
       
 //      
-        monto.setPreferredSize(new Dimension(60, 30));
-        Iconos.scaleImage("ok", iconoOkV, 30);
+        montoDes.setPreferredSize(new Dimension(60, 30));
+        Iconos.scaleImage("ok", iconoOkDesarrollo, 30);
        
-        rellenarVentas.add(inventario);
-        rellenarVentas.add(monto);
-        rellenarVentas.add(plataformacb);
-        rellenarVentas.add(iconoOkV);
-        monto2 = monto.getText();
+        rellenarDesarrollo.add(desarrollo);
+        rellenarDesarrollo.add(montoDes);
+        rellenarDesarrollo.add(iconoOkDesarrollo);
         
-        return rellenarVentas;
+        return rellenarDesarrollo;
     }
      
-     
-    public  JPanel rellenarCredito(){
-        rellenarCredito.removeAll();
+    public  JPanel rellenarDeudasPagar(){
+        rellenarDeudasPagar.removeAll();
         
-        rellenarCredito.setBackground(Color.white);
-        Iconos.scaleImage("Ventas", iconoCredito, 40);
-        rellenarCredito.add(iconoCredito);
-        Iconos.scaleImage("ok", iconoOkC, 30);
+        rellenarDeudasPagar.setBackground(Color.white);
+        Iconos.scaleImage("Ventas", iconoDeudasPagar, 40);
+        rellenarDeudasPagar.add(iconoDesarrollo);
+        Iconos.scaleImage("ok", iconoOkDeudas, 30);
         
-        inversor = new JTextField("Prestamista");
-        montoC = new JTextField("$");
-        montoC.setPreferredSize(new Dimension(60, 30));
-        inversor.setPreferredSize(new Dimension(150, 30));      
-        
+        deudas = new JTextField("Deudor");
+        montoDeuda = new JTextField("$");
+        montoDeuda.setPreferredSize(new Dimension(60, 30));
+        deudas.setPreferredSize(new Dimension(150, 30));      
         
         
-        rellenarCredito.add(inversor);
-        rellenarCredito.add(montoC);
         
-        rellenarCredito.add(iconoOkC);
+        rellenarDeudasPagar.add(deudas);
+        rellenarDeudasPagar.add(montoDeuda);
         
-        return rellenarCredito;
+        rellenarDeudasPagar.add(iconoOkDeudas);
+        
+        return rellenarDeudasPagar;
     } 
 
-    public  JPanel rellenarInversion(){
-        rellenarInversion.removeAll();
+    public  JPanel rellenarImpuestoG(){
+        rellenarImpuestoG.removeAll();
         
-        rellenarInversion.setBackground(Color.white);
-        Iconos.scaleImage("Ventas", iconoInversion, 40);
-        rellenarInversion.add(iconoInversion);
-        Iconos.scaleImage("ok", iconoOkI, 30);
-        inversorI = new JTextField("Inversor");
-        montoI = new JTextField("$");
-        montoI.setPreferredSize(new Dimension(60, 30));
-        inversorI.setPreferredSize(new Dimension(150, 30));      
-        
-        
-        
-        rellenarInversion.add(inversorI);
-        rellenarInversion.add(montoI);
-        
-        rellenarInversion.add(iconoOkI);
-        
-        return rellenarInversion;
-    } 
-    
-    public  JPanel rellenarImpuesto(){
-        rellenarImpuesto.removeAll();
-        
-        rellenarImpuesto.setBackground(Color.white);
+        rellenarImpuestoG.setBackground(Color.white);
         Iconos.scaleImage("Ventas", iconoImpuesto, 40);
-        rellenarImpuesto.add(iconoImpuesto);
-        Iconos.scaleImage("ok", iconoOkImp, 30);
+        rellenarImpuestoG.add(iconoImpuesto);
+        Iconos.scaleImage("ok", iconoOkImpuesto, 30);
         impuesto = new JTextField("Impuesto");
-        montoImp = new JTextField("$");
-        montoImp.setPreferredSize(new Dimension(60, 30));
+        montoImpuesto = new JTextField("$");
+        montoImpuesto.setPreferredSize(new Dimension(60, 30));
         impuesto.setPreferredSize(new Dimension(150, 30));      
         
         
         
-        rellenarImpuesto.add(impuesto);
-        rellenarImpuesto.add(montoImp);
+        rellenarImpuestoG.add(impuesto);
+        rellenarImpuestoG.add(montoImpuesto);
+        rellenarImpuestoG.add(iconoOkImpuesto);
         
-        rellenarImpuesto.add(iconoOkImp);
-        
-        return rellenarImpuesto;
-    }
-     
+        return rellenarImpuestoG;
+    } 
     
+    public  JPanel rellenarMantenimiento(){
+        rellenarMantenimiento.removeAll();
+        
+        rellenarMantenimiento.setBackground(Color.white);
+        Iconos.scaleImage("Ventas", iconoMantenimiento, 40);
+        rellenarMantenimiento.add(iconoMantenimiento);
+        Iconos.scaleImage("ok", iconoOkMantenimiento, 30);
+        mantenimiento = new JTextField("Mantenimiento");
+        montoMant = new JTextField("$");
+        montoMant.setPreferredSize(new Dimension(60, 30));
+        mantenimiento.setPreferredSize(new Dimension(150, 30));      
+        
+        
+        
+        rellenarMantenimiento.add(impuesto);
+        rellenarMantenimiento.add(montoMant);
+        rellenarMantenimiento.add(iconoOkMantenimiento);
+        
+        return rellenarMantenimiento;
+    }
+    
+    public  JPanel rellenarSuministros(){
+        rellenarSuministros.removeAll();
+        
+        rellenarSuministros.setBackground(Color.white);
+        Iconos.scaleImage("Ventas", iconoSuministros, 40);
+        rellenarSuministros.add(iconoSuministros);
+        Iconos.scaleImage("ok", iconoOkSuministros, 30);
+        suministros = new JTextField("Suministros");
+        montoSum = new JTextField("$");
+        montoSum.setPreferredSize(new Dimension(60, 30));
+        suministros.setPreferredSize(new Dimension(150, 30));      
+        
+        
+        
+        rellenarSuministros.add(suministros);
+        rellenarSuministros.add(montoSum);
+        rellenarSuministros.add(iconoOkSuministros);
+        
+        return rellenarSuministros;
+    }
+    
+    public  JPanel rellenarPubliidad(){
+        rellenarPubliidad.removeAll();
+        
+        rellenarPubliidad.setBackground(Color.white);
+        Iconos.scaleImage("Ventas", iconoPublicidad, 40);
+        rellenarPubliidad.add(iconoPublicidad);
+        Iconos.scaleImage("ok", iconoOkPublicidad, 30);
+        publicidad = new JTextField("Publicidad");
+        montoPublicidad = new JTextField("$");
+        montoPublicidad.setPreferredSize(new Dimension(60, 30));
+        publicidad.setPreferredSize(new Dimension(150, 30));      
+        
+        
+        
+        rellenarPubliidad.add(publicidad);
+        rellenarPubliidad.add(montoPublicidad);
+        rellenarPubliidad.add(iconoOkPublicidad);
+        
+        return rellenarPubliidad;
+    }
+    
+    public  JPanel rellenarTransporte(){
+        rellenarTransporte.removeAll();
+        
+        rellenarTransporte.setBackground(Color.white);
+        Iconos.scaleImage("Ventas", iconoTransporte, 40);
+        rellenarTransporte.add(iconoTransporte);
+        Iconos.scaleImage("ok", iconoOkTransporte, 30);
+        transporte = new JTextField("Publicidad");
+        montoTrans = new JTextField("$");
+        montoTrans.setPreferredSize(new Dimension(60, 30));
+        transporte.setPreferredSize(new Dimension(150, 30));      
+        
+        
+        
+        rellenarTransporte.add(transporte);
+        rellenarTransporte.add(montoTrans);
+        rellenarTransporte.add(iconoOkTransporte);
+        
+        return rellenarTransporte;
+    }
+        
+    public  JPanel rellenarHonorarios(){
+        rellenarHonorarios.removeAll();
+        
+        rellenarHonorarios.setBackground(Color.white);
+        Iconos.scaleImage("Ventas", iconoHonorarios, 40);
+        rellenarHonorarios.add(iconoHonorarios);
+        Iconos.scaleImage("ok", iconoOkHonorarios, 30);
+        honorarios = new JTextField("Publicidad");
+        montoHonor = new JTextField("$");
+        montoHonor.setPreferredSize(new Dimension(60, 30));
+        honorarios.setPreferredSize(new Dimension(150, 30));      
+        
+        
+        
+        rellenarHonorarios.add(honorarios);
+        rellenarHonorarios.add(montoHonor);
+        rellenarHonorarios.add(iconoOkHonorarios);
+        
+        return rellenarHonorarios;
+    }
+    
+    public  JPanel rellenarReducInv(){
+        rellenarReducInv.removeAll();
+        
+        rellenarReducInv.setBackground(Color.white);
+        Iconos.scaleImage("Ventas", iconoReducInv, 40);
+        rellenarReducInv.add(iconoReducInv);
+        Iconos.scaleImage("ok", iconoReducInv, 30);
+        String[] listaInventario={"inventario","Bici chingona","Bici Chingona 2","Bici Chingona 3"};
+        reducInv = new JComboBox(listaInventario);
+        montoReduc = new JTextField("$");
+        montoReduc.setPreferredSize(new Dimension(60, 30));
+        reducInv.setPreferredSize(new Dimension(150, 30));      
+        
+        
+        
+        rellenarReducInv.add(reducInv);
+        rellenarReducInv.add(montoReduc);
+        rellenarReducInv.add(iconoOkReducInv);
+        
+        return rellenarReducInv;
+    }
+    
+    
+     
 }
